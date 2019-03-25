292d6c07b00ef5de9a09b987aee1036ad1b27943
Dubprocessor <dubprocessor@protonmail.com>
2019-03-25 14:11:25 +0300

    ignore src directory

--- src/compiled/tsx/components/logo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-function Logo() {
-    return (React.createElement("div", { className: "logo" },
-        React.createElement("img", { src: "./assets/img/vectorpaint-zipped_animated.svg", alt: "\u0434\u0435\u0432\u0443\u0448\u043A\u0430 \u0441 \u0430\u0444\u0440\u0438\u043A\u0430\u043D\u0441\u043A\u0438\u043C\u0438 \u043A\u043E\u0441\u0438\u0447\u043A\u0430\u043C\u0438" })));
-}
-exports.Logo = Logo;
-exports.consumers = ["src/compiled/tsx/index.js"];
